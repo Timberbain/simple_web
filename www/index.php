1b62fbe68d5a33beafad387d9efbd2344a5b2883
Jonas Brandvik <TImberbain@gmail.com>
2016-08-12 12:04:09 +0200

    Database
    Added database functionality

--- www/index.php
+++ www/index.php
@@ -3,5 +3,13 @@
     </head>
     <body>
         Hello world!
+        <br/>
+        <?php 
+            phpinfo();
+            $connection = mysqli_connect("mysql", "admin", "password", "mybase");
+            if(!$connection){
+                echo "Unable to connect to database.";
+            }
+        ?>
     </body>
 </html>
